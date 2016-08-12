0306d744022dfd3c5feb29d2ed2d8350d3dfbf42
Van Duc Uy <vducuy@gmail.com>
2016-08-12 07:51:38 +0000

    fix inconsistency public network name

--- 17-controller-launch-private-instance.sh
+++ 17-controller-launch-private-instance.sh
@@ -5,9 +5,9 @@ openstack image list
 openstack network list
 openstack security group list
 
-echo -n "Copy Provider network ID > "
-read PROVIDER_NET_ID
-nova --debug boot --flavor m1.small --image Wily --nic net-id=$PROVIDER_NET_ID --security-group default --key-name mykey wily
+echo -n "Copy Private network ID > "
+read PRIVATE_NET_ID
+nova --debug boot --flavor m1.small --image Wily --nic net-id=$PRIVATE_NET_ID --security-group default --key-name mykey wily
 
 
 openstack server list
